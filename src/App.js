fe2e49c9d50a165706fed72c6d56fd92db662dc3
Supernumerario <supernumerario@gmail.com>
2023-04-13 14:14:12 +0200

    Level 01 Exercise 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [products, setProducts] = useState({
+        web: false,
+        seo: false,
+        ads: false
+    });
+
+    const [total, setTotal] = useState(0);
+
+    function calculateTotal(products) {
+        let total = 0;
+        total = products.web ? total + 500 : total;
+        total = products.seo ? total + 300 : total;
+        total = products.ads ? total + 200 : total;
+        return total;
+    }
+
+    function handleInputpChange(event) {
+        const nextProducts = {...products};
+        let selection = event.target.name;
+        nextProducts[selection] = event.target.checked;
+        try {
+            setProducts({...nextProducts});
+        } catch (error) {
+            console.error(error);
+        }
+        setTotal(calculateTotal(nextProducts));
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <h2>Què vols fer?</h2>
+                <div>
+                    <input type="checkbox" id="web-id" name="web" onClick={handleInputpChange}></input>
+                    <label for="web-id">Una pàgina web (500€)</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="seo-id" name="seo" onClick={handleInputpChange}></input>
+                    <label for="seo-id">Una consultoria SEO (300€)</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="ads-id" name="ads" onClick={handleInputpChange}></input>
+                    <label for="ads-id">Una campanya de Google Ads (200€)</label>
+                </div>
+                <p>Preu: {total}</p>
+            </form>
+        </div>
+    );
 }
 
 export default App;
